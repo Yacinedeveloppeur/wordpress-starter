c7db8d6db05254ba516fce4714de8d68e64ffb5b
Yacine Developpeur <yacine.developpeur@gmail.com>
2022-04-15 09:36:26 +0200

    .

---
+++ wordpress/wp-content/plugins/simply-static/static-files/simply-static-1-1650006641/wp-login.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Se connecter &lsaquo; starter_website &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//cdnjs.cloudflare.com">
+<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="tinySlider-css" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css?ver=5.9.3" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=5.9.3" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="/wp-includes/css/buttons.min.css?ver=5.9.3" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="/wp-admin/css/forms.min.css?ver=5.9.3" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="/wp-admin/css/l10n.min.css?ver=5.9.3" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="/wp-admin/css/login.min.css?ver=5.9.3" type="text/css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+	<link rel="icon" href="/wp-content/uploads/2022/04/cropped-Fichier-1@4x-32x32.png" sizes="32x32">
+<link rel="icon" href="/wp-content/uploads/2022/04/cropped-Fichier-1@4x-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="/wp-content/uploads/2022/04/cropped-Fichier-1@4x-180x180.png">
+<meta name="msapplication-TileImage" content="httplocalhost/wordpress-starter/wordpress/wp-content/uploads/2022/04/cropped-Fichier-1@4x-270x270.png">
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-fr-fr">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://fr.wordpress.org/">Propulsé par WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="httplocalhost/wordpress-starter/wordpress/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Identifiant ou adresse e-mail</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Mot de passe</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Afficher le mot de passe">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Se souvenir de moi</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter">
+									<input type="hidden" name="redirect_to" value="httplocalhost/wordpress-starter/wordpress/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="/">&larr; Aller sur starter_website</a>		</p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="/politique-de-confidentialite/">Politique de confidentialité</a></div>	</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">Langue</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="fr_FR" lang="fr" selected data-installed="1">Français</option></select>
+					
+					
+					
+						<input type="submit" class="button" value="Modifier">
+
+					</form>
+				</div>
+				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js?ver=5.9.3" id="tinySlider-js"></script>
+<script type="text/javascript" src="/wp-content/themes/startertheme/blocks/lastposts/slider.js?ver=1.0" id="slider-js"></script>
+<script type="text/javascript" src="/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"httplocalhost\/wordpress-starter\/wordpress\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c" id="wp-hooks-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Force du mot de passe inconnue.","short":"Très faible","bad":"Faible","good":"Moyenne","strong":"Forte","mismatch":"Non concordance"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2022-04-06 14:13:01+0000","generator":"GlotPress\/3.0.0-rc.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"fr"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s est obsolète depuis la version %2$s ! Utilisez %3$s à la place. Pensons à écrire du code plus inclusif."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+</script>
+<script type="text/javascript" src="/wp-admin/js/password-strength-meter.min.js?ver=5.9.3" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wordpress-starter\/wordpress\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="/wp-includes/js/wp-util.min.js?ver=5.9.3" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"0f7b1549a5"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2022-04-06 14:13:01+0000","generator":"GlotPress\/3.0.0-rc.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"fr"},"Your new password has not been saved.":["Votre nouveau mot de passe n’a pas été enregistré."],"Hide":["Masquer"],"Show":["Afficher"],"Confirm use of weak password":["Confirmer l’utilisation du mot de passe faible"],"Hide password":["Cacher le mot de passe"],"Show password":["Afficher le mot de passe"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+</script>
+<script type="text/javascript" src="/wp-admin/js/user-profile.min.js?ver=5.9.3" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
